d5fc4372c1ed0b14f228b309664fa8d789b3b90d
oc2018 <ontime.c23@gmail.com>
2022-01-12 20:03:33 +0300

    edited commit

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports'
+import './blog.css';
 
 const Blog = () => {
     return (
-        <div>
-            Blog
+        <div className='gpt3__blog section__padding' id='blog'>
+            <div className="gpt3__blog-heading">
+                <h1 className="gradient__text">
+                    A lot is happening, We are blogging about it.
+                </h1>
+            </div>
+            <div className="gpt3__blog-container">
+                <div className="gpt3__blog-container_groupA">
+                <Article imgUrl={blog01} date='sep 26, 2021' title='GPT3 and Open Ai is the future. Lets explore how it is' />
+                </div> 
+                <div className="gpt3__blog-container_groupB">
+                <Article imgUrl={blog02} date='sep 26, 2021' title='GPT3 and Open Ai is the future. Lets explore how it is' />
+                <Article imgUrl={blog03} date='sep 26, 2021' title='GPT3 and Open Ai is the future. Lets explore how it is' />
+                <Article imgUrl={blog04} date='sep 26, 2021' title='GPT3 and Open Ai is the future. Lets explore how it is' />
+                <Article imgUrl={blog05} date='sep 26, 2021' title='GPT3 and Open Ai is the future. Lets explore how it is' />
+                </div>
+            </div>
         </div>
     )
 }
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import './footer.css';
+import gpt3Logo from '../../assets/logo.svg';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className='gpt3__footer section__padding'>
+            <div className="gpt3__footer-heading">
+                <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+            </div>
+            <div className="gpt3__footer-btn">
+                <p>Request Early Acceess</p>
+            </div>
+            <div className="gpt3__footer-links">
+                <div className="gpt3__footer-links_logo">
+                    <img src={gpt3Logo} alt="logo" />
+                    <p>Nkubu - Mitunguu Rd. Nkubu Meru Kenya</p>
+                </div>
+                    <div className="gpt3__footer-links_div">
+                        <h4>Links</h4>
+                        <p>Overons</p>
+                        <p>Social Media</p>
+                        <p>Counters</p>
+                        <p>Contacts</p>
+                    </div>
+                    <div className="gpt3__footer-links_div">
+                        <h4>Company</h4>
+                        <p>Terms & Condations</p>
+                        <p>Privacy Policy</p>
+                        <p>Contacts</p>
+                    </div>
+                    <div className="gpt3__footer-links_div">
+                        <h4>Get in touch</h4>
+                        <p>Nkubu - Mitunguu Rd. Nkubu Meru Kenya</p>
+                        <p>0725900078</p>
+                        <p>info@ontime_c.com</p>
+                    </div>
+            </div>
+            <div className="gtp3__footer-copyright">
+                <p>&#169; 2021 GPT-3, All rights reserved.</p>
+            </div>
         </div>
     )
 }
